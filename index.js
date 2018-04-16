89401407604bea373a5d3a16ff8be3098d764df3
Stephanos Bacon <stephanosbacon@gmail.com>
2018-04-16 19:45:18 -0400

    more

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  console.log(req)
-  res.writeHead(200, {'Content-Type': 'text/json'});
-  // res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
+  let body = [];
+  req.on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    console.log(body);
+    console.log(req);
+    res.writeHead(200, {'Content-Type': 'text/json'});
+    res.end(); //end the response
+  });
 }).listen(8080); //the server object listens on port 8080
 
 
