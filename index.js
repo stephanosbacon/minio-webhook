366c3881e5dfa9de22cbc051d0fe4bd92c18fde1
Stephanos Bacon <stephanosbacon@gmail.com>
2018-04-20 14:10:33 -0400

    coughing and wheezing now

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 // This is a little relay service because I can't see how to get minio
 // to kick authenticated webhooks.
 
-var wskService = process.env.WSK_SERVICE || 'nginx';
+var wskService = process.env.WSK_SERVICE || 'nginx.openwhisk.svc';
 var wskAuth = process.env.WSKAUTH;
 
 http.createServer(function (req, res) {
@@ -13,14 +13,27 @@ http.createServer(function (req, res) {
   }).on('end', () => {
     body = Buffer.concat(body).toString();
     console.log(body);
+    console.log('that was the body');
     res.writeHead(200, {'Content-Type': 'text/json'});
     res.end(); //end the response
+    console.log('service for openwhisk: ' + wskService);
+
+    let postData = JSON.stringify({
+      'message': body
+    });
+
+    console.log('postData: ' + postData);
 
     let options = {
       hostname: wskService,
+      path: '/api/v1/namespaces/_/triggers/mt',
       port: 80,
       auth: wskAuth,
-      method: 'POST'
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(postData)
+      }
     };
 
     let req = http.request(options, (res) => {
@@ -35,9 +48,11 @@ http.createServer(function (req, res) {
       });
     });
 
-    req.write({
-      message: body
+    req.on('error', (e) => {
+      console.error(`problem with request: ${e.message}`);
     });
+
+    req.write(postData);
     req.end();
   });
 }).listen(8080); //the server object listens on port 8080
